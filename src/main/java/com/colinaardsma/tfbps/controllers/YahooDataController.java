c9e8132faa572c8c9aba3ffda81b849c7081d954
Colin Aardsma <colin.aardsma@gmail.com>
2016-11-25 12:46:45 -0600

    cleaned up a bit

--- src/main/java/com/colinaardsma/tfbps/controllers/YahooDataController.java
+++ src/main/java/com/colinaardsma/tfbps/controllers/YahooDataController.java
@@ -1,35 +1,26 @@
 package com.colinaardsma.tfbps.controllers;
 
-import java.io.IOException;
 import java.io.StringReader;
-import java.net.MalformedURLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import com.colinaardsma.tfbps.models.util.OAuthPost;
 import com.colinaardsma.tfbps.models.util.SignPostTest;
 
-import oauth.signpost.exception.OAuthCommunicationException;
-import oauth.signpost.exception.OAuthExpectationFailedException;
-import oauth.signpost.exception.OAuthMessageSignerException;
-
 @Controller
-public class OAuthTestController extends AbstractController {
+public class YahooDataController extends AbstractController {
 	
 	@RequestMapping(value = "/oauthtest")
     public String oauthtest(Model model, HttpServletRequest request) {
@@ -61,8 +52,7 @@ public class OAuthTestController extends AbstractController {
 		    InputSource is = new InputSource(new StringReader(xmlData));
 		    Document document = builder.parse(is);
 			
-			//Iterating through the nodes and extracting the data.
-		    
+			//Iterating through the nodes and extracting the data.	    
 		    NodeList leagueList = document.getElementsByTagName("league");
 		    for (int i = 0; i < leagueList.getLength(); i++) {
 		    	Node leagueNode = leagueList.item(i);
@@ -141,46 +131,103 @@ public class OAuthTestController extends AbstractController {
 
         return "oauthtest";
     }
-	
-//	@RequestMapping(value = "/yahoouserlookup", method = RequestMethod.GET)
-//    public String yahoouserlookupform(Model model, HttpServletRequest request) {
-//		return "yahoouserlookup";
+
+//	@RequestMapping(value = "/oauthtest")
+//	public String oauthtest(Model model, HttpServletRequest request) {
+//		// this version (POST) is preferred to the GET version (above), but need to figure out how to allow individual user login via yahoo
+////		String league_key = request.getParameter("league_key");		
+//		String league_key = "357.l.3091";
+//		String leagueName = new String();
+//		String leagueURL = new String();
+//		String leagueScoringType = new String();
+//		String error = new String();
+//		
+//		try {
+//			if (league_key != null) {
+//
+//				// yahoo query variables
+////				https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.leagues.standings%20where%20league_key%3D'238.l.627060'&diagnostics=true
+//				String url = "https://query.yahooapis.com/v1/yql";
+//				String query = "select * from fantasysports.leagues.standings where league_key='357.l.3091'";
+//
+//				// set xml data string
+//				String xmlData = OAuthPost.postConnection(url, query);
+//
+//				// parse xml data
+//				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//				DocumentBuilder builder = factory.newDocumentBuilder();
+//				InputSource is = new InputSource(new StringReader(xmlData));
+//				Document document = builder.parse(is);
+//
+//				// extract relevant data
+//				// iterate through the nodes and extract the data.	    
+//			    NodeList leagueList = document.getElementsByTagName("league");
+//			    for (int i = 0; i < leagueList.getLength(); i++) {
+//			    	Node leagueNode = leagueList.item(i);
+//			    	if (leagueNode.getNodeType() == Node.ELEMENT_NODE) {
+//					    Element leagueElement = (Element) leagueNode;
+//						leagueName = leagueElement.getElementsByTagName("name").item(0).getTextContent();
+//						leagueURL = leagueElement.getElementsByTagName("url").item(0).getTextContent();
+//						leagueScoringType = leagueElement.getElementsByTagName("scoring_type").item(0).getTextContent();
+//
+//			    	}	
+//			    }
+//			}
+//		} catch (NullPointerException e) {
+//			e.printStackTrace();
+//			error = "Yahoo! league key not found.";
+//		} catch (IOException e2) {
+//			e2.printStackTrace();
+//			error = "Yahoo! league key not found.";
+//		} catch (Exception e3) {
+//			e3.printStackTrace();
+//		}
+//		
+//		model.addAttribute("leagueName", leagueName);
+//		model.addAttribute("leagueURL", leagueURL);
+//		model.addAttribute("leagueScoringType", leagueScoringType);
+//		model.addAttribute("error", error);
+//			
+//		return "oauthtest";
 //	}
-		
+	
 	@RequestMapping(value = "/yahoouserlookup")
-	public String yahoouserlookup(Model model, HttpServletRequest request) throws OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException, IOException, SAXException, ParserConfigurationException {
-//		BasicConfigurator.configure();
+	public String yahoouserlookup(Model model, HttpServletRequest request) {
 
 		String yahooUsername = request.getParameter("yahooUsername");
 		String yahooGUID = new String();
+		String error = new String();
 		
-		if (yahooUsername != null) {
+		try {
+			if (yahooUsername != null) {
 
-			// yahoo query variables
-			String url = "https://query.yahooapis.com/v1/yql";
-			String query = "select * from yahoo.identity where yid='" + yahooUsername + "'";
+				// yahoo query variables
+				String url = "https://query.yahooapis.com/v1/yql";
+				String query = "select * from yahoo.identity where yid='" + yahooUsername + "'";
 
-			// set xml data string
-			String xmlData = OAuthPost.postConnection(url, query);
+				// set xml data string
+				String xmlData = OAuthPost.postConnection(url, query);
 
-			// parse xml data
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			InputSource is = new InputSource(new StringReader(xmlData));
-			Document document = builder.parse(is);
+				// parse xml data
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				InputSource is = new InputSource(new StringReader(xmlData));
+				Document document = builder.parse(is);
 
-			// extract relevant data
-			try {
+				// extract relevant data
 				yahooGUID = document.getElementsByTagName("guid").item(0).getTextContent();
-			} catch (NullPointerException e) {
-				e.printStackTrace();
 			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			error = "Yahoo! username not found.";
+		} catch (Exception e2) {
+			e2.printStackTrace();
 		}
 		
 		model.addAttribute("yahooGUID", yahooGUID);
+		model.addAttribute("error", error);
 			
 		return "yahoouserlookup";
 	}
-
 
 }
--- src/main/java/com/colinaardsma/tfbps/models/util/OAuthPost.java
+++ src/main/java/com/colinaardsma/tfbps/models/util/OAuthPost.java
@@ -19,89 +19,78 @@ import oauth.signpost.exception.OAuthMessageSignerException;
 import oauth.signpost.http.HttpParameters;
 
 public class OAuthPost {
-	
 
-
-	
 	public static String postConnection (String site, String query) throws IOException, OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException {
 
-	      final String consumer_key = "dj0yJmk9YWE1SnlhV0lUbndoJmQ9WVdrOU9FUmhUelV6TkdVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lMQ--";
-	      final String consumer_secret = "55d6606ea0bec9a1468d3ea01bbf1c9991dbf93f";
+		// declare oauth variables
+		final String consumer_key = "dj0yJmk9YWE1SnlhV0lUbndoJmQ9WVdrOU9FUmhUelV6TkdVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lMQ--";
+		final String consumer_secret = "55d6606ea0bec9a1468d3ea01bbf1c9991dbf93f";
 
-	      
-	        //Create an HttpURLConnection and add some headers
-	      URL url = new URL(site + "?" + URLEncoder.encode(query, "UTF-8"));
-	     HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-	     urlConnection.setRequestProperty("Accept", "application/xml");
-	     urlConnection.setRequestMethod("POST");
-	     urlConnection.setDoOutput(true);
-	 
-	        //Build the list of parameters
-	      HttpParameters params =  new HttpParameters();
-	      params.put("q", query);
-	      
-	        //Sign the request
-	      //The key to signing the POST fields is to add them as additional parameters, but already percent-encoded; and also to add the realm header.
-	        OAuthConsumer dealabsConsumer = new DefaultOAuthConsumer(consumer_key, consumer_secret);
-	        HttpParameters doubleEncodedParams =  new HttpParameters();
-	        Iterator<String> iter = params.keySet().iterator();
-	        while (iter.hasNext()) {
-	            String key = iter.next();
-	            doubleEncodedParams.put(key, OAuth.percentEncode(params.getFirst(key)));
-	        }
-	        doubleEncodedParams.put("realm", site);
-	      dealabsConsumer.setAdditionalParameters(doubleEncodedParams);
-	        dealabsConsumer.sign(urlConnection);
-	         
-	        //Create the POST payload
-	        StringBuilder sb = new StringBuilder();
-	        iter = params.keySet().iterator();
-	        for (int i = 0; iter.hasNext(); i++) {
-	            String param = iter.next();
-	            if (i > 0) {
-	                sb.append("&");
-	            }
-	            sb.append(param);
-	            sb.append("=");
-	            sb.append(OAuth.percentEncode(params.getFirst(param)));
-	        }
-	         
-	        //Send the payload to the connection
-	        String formEncoded = sb.toString();;
-	        OutputStreamWriter outputStreamWriter = null;
-	        try {
-	            outputStreamWriter = new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8");
-	            outputStreamWriter.write(formEncoded);
-	        } finally {
-	            if (outputStreamWriter != null) {
-	                outputStreamWriter.close();
-	            }
-	        }
-	         
-	        //Send the request and read the output
-	        String xmlData = new String();
-	       try {
-	           System.out.println("Response: " + urlConnection.getResponseCode() + " " + urlConnection.getResponseMessage());
-	          InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-	           String inputStreamString = new Scanner(in,"UTF-8").useDelimiter("\\A").next();
-	            System.out.println(inputStreamString);
-	            
-//	            BufferedReader rd = new BufferedReader(new InputStreamReader(responseCode==200?uc.getInputStream():uc.getErrorStream()));
-//                StringBuffer sb = new StringBuffer();
-//                String line;
-//                while ((line = rd.readLine()) != null) {
-//                    sb.append(line);
-//                }
-//                rd.close();
-//                setResponseBody(sb.toString());
-	    		xmlData = inputStreamString;
+		//Create an HttpURLConnection and add some headers
+		URL url = new URL(site + "?q=" + URLEncoder.encode(query, "UTF-8"));
+		HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+		urlConnection.setRequestProperty("Accept", "application/xml");
+		urlConnection.setRequestMethod("POST");
+		urlConnection.setDoOutput(true);
 
-	      }
-	       finally {
-	           urlConnection.disconnect();
-	           
-	     }
-   		return xmlData;
-	 
+		//Build the list of parameters
+		HttpParameters params =  new HttpParameters();
+		params.put("q", query);
+
+		//Sign the request
+		//The key to signing the POST fields is to add them as additional parameters, but already percent-encoded; and also to add the realm header.
+		OAuthConsumer dealabsConsumer = new DefaultOAuthConsumer(consumer_key, consumer_secret);
+		HttpParameters doubleEncodedParams =  new HttpParameters();
+		Iterator<String> iter = params.keySet().iterator();
+		while (iter.hasNext()) {
+			String key = iter.next();
+			doubleEncodedParams.put(key, OAuth.percentEncode(params.getFirst(key)));
+		}
+		doubleEncodedParams.put("realm", site);
+		dealabsConsumer.setAdditionalParameters(doubleEncodedParams);
+		dealabsConsumer.sign(urlConnection);
+
+		//Create the POST payload
+		StringBuilder sb = new StringBuilder();
+		iter = params.keySet().iterator();
+		for (int i = 0; iter.hasNext(); i++) {
+			String param = iter.next();
+			if (i > 0) {
+				sb.append("&");
+			}
+			sb.append(param);
+			sb.append("=");
+			sb.append(OAuth.percentEncode(params.getFirst(param)));
+		}
+
+		//Send the payload to the connection
+		String formEncoded = sb.toString();
+		OutputStreamWriter outputStreamWriter = null;
+		try {
+			outputStreamWriter = new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8");
+			outputStreamWriter.write(formEncoded);
+		} finally {
+			if (outputStreamWriter != null) {
+				outputStreamWriter.close();
+			}
+		}
+
+		//Send the request and read the output
+		String xmlData = new String();
+		try {
+			System.out.println("Connecting to: " + site + "?" + sb);
+			System.out.println("Response: " + urlConnection.getResponseCode() + " " + urlConnection.getResponseMessage());
+			InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+			Scanner scanner = new Scanner(in,"UTF-8");
+			xmlData = scanner.useDelimiter("\\A").next();;
+			scanner.close();
+			System.out.println(xmlData);
+		}
+		finally {
+			urlConnection.disconnect();
+		}
+		
+		return xmlData;
+
 	}
 }
